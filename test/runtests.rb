fa9a9d90b05e2b5b5fccb725910a234de8ee98a8
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-06-14 15:29:41 -0600

    new bakery, and use service_testing port for tests

--- test/runtests.rb
+++ test/runtests.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
-require File.join(File.dirname(__FILE__), 'bp_service_runner')
+require File.join(File.dirname(File.dirname(File.expand_path(__FILE__))),
+                  'external/built/share/service_testing/bp_service_runner')
 require 'uri'
 require 'test/unit'
 require 'open-uri'
